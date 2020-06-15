e13f448ef354d464d9bf939ff255d7ca9e31dde0
Tituo <784377707@qq.com>
2020-06-15 20:07:03 +0800

    springcloud：Hystrix服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/tituo/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/tituo/springcloud/controller/PaymentController.java
@@ -50,4 +50,14 @@ public class PaymentController {
     }
 
 
+    //================服务熔断===============//
+
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id)
+    {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("****result: "+result);
+        return result;
+    }
+
 }
