be478d76bfe68d2c1d1141c9cd4abffe4d6e4625
Tituo <784377707@qq.com>
2020-06-15 22:52:25 +0800

    springcloud：gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/tituo/springcloud/config/GateWayConfig.java
@@ -0,0 +1,27 @@
+package com.tituo.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fuqm
+ * @create 2020-06-15
+ */
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_tituo",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+
+}
---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author fuqm
+ * @create 2020-06-15
+ */
+public class T2 {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);    //2020-06-15T21:54:47.950+08:00[Asia/Shanghai]
+    }
+}
