74441e1fb5b91bd94c4b09ff8c75c1157426c6b8
Tituo <784377707@qq.com>
2020-06-30 23:34:05 +0800

    springcloudAlibab

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/tituo/springcloud/NacosProviderDemoApplication.java
@@ -0,0 +1,17 @@
+package com.tituo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author fuqm
+ * @create 2020-06-17
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderDemoApplication {
+    public static void main(String[] args) {
+            SpringApplication.run(NacosProviderDemoApplication .class, args);
+        }
+}
